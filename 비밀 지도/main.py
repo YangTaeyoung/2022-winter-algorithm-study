09246287e893c4a804a320e99fe07dc68b8cd23a
Taeyoung Yang <0130yang@gmail.com>
2022-01-06 04:18:23 +0900

    (양태영) 프로그래머스 - 비밀지도 풀이

---
+++ 비밀 지도/main.py
@@ -0,0 +1,8 @@
+# 프로그래머스 - 비밀지도 풀이
+# https://programmers.co.kr/learn/courses/30/lessons/17681
+# 그냥 가끔 그럴 때가 있잖음.. 어떻게든 짧게 적고 싶어질 때
+def solution(n, arr1, arr2):
+    answer = []
+    for i in range(n):
+        answer.append(str(bin(arr1[i] | arr2[i]))[2:].zfill(n).replace("1", "#").replace("0", " "))
+    return answer
