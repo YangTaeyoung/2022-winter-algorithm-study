6172ebe8cd87884d3f87756e3c071df87bf9a800
Taeyoung Yang <0130yang@gmail.com>
2022-01-07 00:50:02 +0900

    (양태영) 프로그래머스 오픈채팅방 문제 풀이.

---
+++ 오픈 채팅방/main.py
@@ -0,0 +1,25 @@
+# 프로그래머스 - 오픈 채팅방 문제
+# https://programmers.co.kr/learn/courses/30/lessons/42888
+LEAVE = False
+ENTER = True
+
+
+def solution(record):
+    answer = list()
+    user_dict = dict()
+    logs = []
+    for r in record:
+        operations = r.split(" ")
+        if operations[0] == "Enter":
+            user_dict[operations[1]] = operations[2]
+            logs.append([ENTER, operations[1]])
+        elif operations[0] == "Change":
+            user_dict[operations[1]] = operations[2]
+        elif operations[0] == "Leave":
+            logs.append([LEAVE, operations[1]])
+    for log in logs:
+        if log[0]:
+            answer.append(f"{user_dict[log[1]]}님이 들어왔습니다.")
+        else:
+            answer.append(f"{user_dict[log[1]]}님이 나갔습니다.")
+    return answer
