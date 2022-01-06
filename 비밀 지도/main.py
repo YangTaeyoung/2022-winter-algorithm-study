619c1419e38cffc2c9174b2fd24ecaa4328acc5f
--replace-all <--replace-all>
2022-01-06 15:23:00 +0900

    한줄 챌린지의 서막.

--- 비밀 지도/main.py
+++ 비밀 지도/main.py
@@ -2,7 +2,4 @@
 # https://programmers.co.kr/learn/courses/30/lessons/17681
 # 그냥 가끔 그럴 때가 있잖음.. 어떻게든 짧게 적고 싶어질 때
 def solution(n, arr1, arr2):
-    answer = []
-    for i in range(n):
-        answer.append(str(bin(arr1[i] | arr2[i]))[2:].zfill(n).replace("1", "#").replace("0", " "))
-    return answer
+    return [str(bin(arr1[i] | arr2[i]))[2:].zfill(n).replace("1", "#").replace("0", " ") for i in range(n)]
