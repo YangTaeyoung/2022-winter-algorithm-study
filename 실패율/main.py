6230797d181e9036a3e9519a0279b50cb064f043
--replace-all <--replace-all>
2022-01-06 15:25:24 +0900

    (양태영) 코드 단순화

--- 실패율/main.py
+++ 실패율/main.py
@@ -2,15 +2,16 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42889
 
 def solution(N: int, stages: list):
+    # 실패한 사람 수를 리스트에 각 스테이지 마다 담음
     fail_counts = [0 for i in range(0, N + 1)]
     for stage in stages:
         fail_counts[stage - 1] += 1
+    # 실패율을 계산하여 스테이지는 키, 실패율은 값에 담음
     fail_rates = {}
     for i in range(len(fail_counts) - 1):
         if total := sum(fail_counts[i:]):
             fail_rates[i] = fail_counts[i] / total
         else:
             fail_rates[i] = 0
-    sorted_list = sorted(fail_rates.items(), key=lambda item: item[1], reverse=True)
-    answer = [item[0] + 1 for item in sorted_list]
-    return answer
+    # 정렬하여 키값(스테이지)만 리스트에 담아 반환
+    return [item[0] + 1 for item in sorted(fail_rates.items(), key=lambda item: item[1], reverse=True)]
