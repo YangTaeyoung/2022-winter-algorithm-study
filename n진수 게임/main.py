2ca8f584b3bfe91e003039530e26c62d56bb75e8
--replace-all <--replace-all>
2022-02-02 18:43:09 +0900

    주석 추가

--- n진수 게임/main.py
+++ n진수 게임/main.py
@@ -3,6 +3,7 @@
 num_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
 
 
+# num을 digit 진수로 변환
 def get_n_digit(num: int, digit: int):
     mok = num
     result = list()
@@ -20,15 +21,17 @@ def get_n_digit(num: int, digit: int):
 
 def solution(n, t, m, p):
     answer = list()
-    digits = ""
+    digits = ""  # 구할 진수 숫자가 들어갈 곳임
     i = 0
+    # 진수 변환 후 숫자 붙이는 과정 1,2,3,4 => 0011011100
     while len(digits) < t * m + p - 1:
         digits += get_n_digit(i, n)
         i += 1
-    print(digits)
+    # 자기 순서에 해당하는 수만 answer 리스트에 넣는 과정
     for i in range(t):
         answer.append(digits[i * m + p - 1])
 
     return "".join(answer)
 
-print(solution(16,16,2,2))
+
+print(solution(16, 16, 2, 2))
